9242835277b62df39f5963569eee2d23ce13e716
Beresnev <Beresnev@gmail.com>
2025-03-10 20:47:14 +0500

    finished layout for the second form pages

--- app/ui/forms/calendar.tsx
+++ app/ui/forms/calendar.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useEffect, useRef } from 'react';
 import { Options, Calendar } from 'vanilla-calendar-pro';
-import { getDate } from 'vanilla-calendar-pro/utils';
 import 'vanilla-calendar-pro/styles/index.css';
 import CalendarSvg from '../svg-icons/calendar';
 
